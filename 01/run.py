1ba7485a63be097ffe063ed3f62462d909f1b89b
Ciprian Grigore <grigoresc@outlook.com>
2021-12-01 09:17:51 +0200

    day 1

---
+++ 01/run.py
@@ -0,0 +1,16 @@
+l = [int(line.strip()) for line in open("input.txt", 'r')]
+
+cnt = 0
+for idx in range(1, len(l)):
+    if l[idx] > l[idx-1]:
+        cnt += 1
+print(cnt)
+# 1752
+
+cnt = 0
+for idx in range(3, len(l)):
+    if l[idx]+l[idx-1]+l[idx-2] > l[idx-1]+l[idx-2]+l[idx-3]:
+        cnt += 1
+
+print(cnt)
+# 1781
