e2d4692a64df184382a8e141117ec5e2042c7bc8
Ciprian Grigore <grigoresc@outlook.com>
2021-12-06 06:24:46 +0200

    day 5 simplify

--- 05/run.py
+++ 05/run.py
@@ -1,55 +1,45 @@
 import re
+from collections import defaultdict
 
 
-def Parse():
-    # inp = [line.strip() for line in open('sample.txt', 'r')]
-    inp = [line.strip() for line in open('input.txt', 'r')]
-
-    for l in inp:
-        (x1, y1, x2, y2) = list(map(int, re.findall(r'\d+', l)))
-        yield (x1, y1, x2, y2)
+def parse(line):
+    (x1, y1, x2, y2) = list(map(int, re.findall(r'\d+', line)))
+    return x1, y1, x2, y2
 
 
-vents = list(Parse())
-c = set()  # covered
-c2 = set()  # covered at least two times
-
-
-def Add(x, y):
-    global c, c2
-    if (x, y) in c:
-        c2.add((x, y))
-    else:
-        c.add((x, y))
-
-
-def Part1(vents):
+def part1():
     for x1, y1, x2, y2 in vents:
         if x1 == x2:
             incy = 1 if y1 < y2 else -1
-            for y in range(y1, y2+incy, incy):
-                Add(x1, y)
+            for y in range(y1, y2 + incy, incy):
+                d[(x1, y)] += 1
         if y1 == y2:
             incx = 1 if x1 < x2 else -1
-            for x in range(x1, x2+incx, incx):
-                Add(x, y1)
+            for x in range(x1, x2 + incx, incx):
+                d[(x, y1)] += 1
 
 
-def Part2(vents):
+def part2():
     for x1, y1, x2, y2 in vents:
-        if abs(x2-x1) == abs(y2-y1):
+        if abs(x2 - x1) == abs(y2 - y1):
             incx = 1 if x1 < x2 else -1
             incy = 1 if y1 < y2 else -1
-            y = y1
-            for x in range(x1, x2+incx, incx):
-                Add(x, y)
-                y += incy
+            for x, y in zip(range(x1, x2 + incx, incx), range(y1, y2 + incy, incy)):
+                d[(x, y)] += 1
 
 
-Part1(vents)
-print(len(c2))
+# inp = [line.strip() for line in open('sample.txt', 'r')]
+inp = [line.strip() for line in open('input.txt', 'r')]
+
+vents = list(map(parse, inp))
+d = defaultdict(int)
+
+part1()
+sln1 = list(filter(lambda x: x > 1, d.values()))
+print(len(sln1))
 # 5442
 
-Part2(vents)
-print(len(c2))
+part2()
+sln2 = list(filter(lambda x: x > 1, d.values()))
+print(len(sln2))
 # 19571
