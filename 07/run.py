52a157eeafa7b558e0744d9f42840a0f5341a1ee
Ciprian Grigore <grigoresc@outlook.com>
2021-12-07 23:38:47 +0200

    day 7 simplify

--- 07/run.py
+++ 07/run.py
@@ -1,42 +1,24 @@
-import re
-from collections import defaultdict
-
 # inp = [line.strip() for line in open('sample.txt', 'r')]
 inp = [line.strip() for line in open('input.txt', 'r')]
 
-
-def parse(line):
-    vals = list(map(int, re.findall(r'\d+', line)))
-    return vals
-
-
-lst = parse(inp[0])
+lst = list(map(int, inp[0].split(',')))
 f = set(lst)
-dist = defaultdict(int)
 
 
 def cost(d):
-    if dist[d] > 0:
-        return dist[d]
-    a = 0
-    for i in range(1, d + 1):
-        a += i
-    dist[d] = a
-    return a
+    return int(d * (d + 1) / 2)
 
 
 ms = 100000000000000
 for p in range(min(f), max(f)):
     s = sum(map(lambda x: abs(p - x), lst))
-    if s < ms:
-        ms = s
+    ms = min(ms, s)
 print(ms)
 # 340052
 
 ms = 100000000000000
 for p in range(min(f), max(f)):
     s = sum(map(lambda x: cost(abs(p - x)), lst))
-    if s < ms:
-        ms = s
+    ms = min(ms, s)
 print(ms)
 # 92948968
