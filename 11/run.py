f6e9cd4d4ef8158d4c5997526d2742eae019b65f
Ciprian Grigore <grigoresc@outlook.com>
2021-12-11 08:00:56 +0200

    day 11

--- 11/run.py
+++ 11/run.py
@@ -1,36 +1,30 @@
-inp = [line.strip() for line in open('sample.txt', 'r')]
-
+# inp = [line.strip() for line in open('sample.txt', 'r')]
 inp = [line.strip() for line in open('input.txt', 'r')]
 
 
 def pos(x, y):
-    for nx, ny in [(x + 1, y), (x - 1, y), (x, y - 1), (x, y + 1), (x + 1, y + 1), (x + 1, y - 1), (x - 1, y + 1),
-                   (x - 1, y - 1)]:
-        if 0 <= nx < len(inp) and 0 <= ny < len(inp[0]):
+    for nx, ny in [(x + 1, y), (x - 1, y), (x, y - 1), (x, y + 1),
+                   (x + 1, y + 1), (x + 1, y - 1), (x - 1, y + 1), (x - 1, y - 1)]:
+        if 0 <= nx < H and 0 <= ny < W:
             yield nx, ny
 
 
-# cp = inp.copy()
-e = list()
-for x in range(0, len(inp)):
-    ei = []
-    for y in range(0, len(inp[0])):
-        ei.append(int(inp[x][y]))
-    e.append(ei)
-
+(H, W) = (len(inp), len(inp[0]))
+e = list(map(lambda x: list(map(int, x)), inp))
 cnt = 0
-rounds = 100
-for _ in range(rounds):
-    # cp = e.copy()
-    for x in range(0, len(inp)):
-        for y in range(0, len(inp[0])):
+r = 0
+
+while True:
+    r += 1
+    for x in range(H):
+        for y in range(W):
             e[x][y] += 1
 
     flashes = set()
     while True:
         found = False
-        for x in range(0, len(inp)):
-            for y in range(0, len(inp[0])):
+        for x in range(H):
+            for y in range(W):
                 if e[x][y] > 9:
                     if not (x, y) in flashes:
                         flashes.add((x, y))
@@ -39,8 +33,15 @@ for _ in range(rounds):
                             e[nx][ny] += 1
         if not found:
             break
+
     for (x, y) in flashes:
         e[x][y] = 0
+
     cnt += len(flashes)
-    # break
-# 1585
+
+    if r == 100:
+        print("sln1", cnt)  # 1585
+
+    if len(flashes) == H * W:
+        print("sln2", r)  # 381
+        break
