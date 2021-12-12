098f06df38c418eec679b25409d193bae9c984a9
Ciprian Grigore <grigoresc@outlook.com>
2021-12-12 09:51:22 +0200

    day 12

--- 12/run.py
+++ 12/run.py
@@ -7,36 +7,38 @@ def parse(line):
     return s, e
 
 
-def search(start, rpaths: set, smalls: list, dup: int, a: list):
+# smalls - already small caves visited
+# dup - flag indicating whether a small cave was already visited twice
+# a - accumulate the whole path (just for printing it..)
+def search(cave, smalls: list, dup: int, a: list):
     global cnt
-    if start.lower() == start:
-        smalls.append(start)
-    if start == 'end':
+    global paths
+    if cave.lower() == cave:
+        smalls.append(cave)
+    if cave == 'end':
         cnt += 1
         return
-    starts = [x for x in rpaths if x[0] == start or x[1] == start]
+    starts = [x for x in paths if x[0] == cave or x[1] == cave]
     for si in starts:
-        next = si[1] if si[0] == start else si[0]
-        nextdup = dup
-        if next in smalls:
-            if nextdup == 0 and next not in ['start', 'end']:
-                nextdup = 1
+        ncave = si[1] if si[0] == cave else si[0]  # next cave
+        ndup = dup  # next duplicate flag
+        if ncave in smalls:
+            if ndup == 0 and ncave not in ['start', 'end']:
+                ndup = 1
             else:
                 continue
-        npaths = rpaths.copy()
         na = a.copy()
-        if next.lower() == next:
-            na.append(next)
-        search(next, npaths, smalls.copy(), nextdup, na)
+        na.append(ncave)
+        search(ncave, smalls.copy(), ndup, na)
 
 
 lst = list(map(parse, inp))
 paths = set(lst)
 
 cnt = 0
-search('start', paths, [], 1, ['start'])
+search('start', [], 1, ['start'])
 print(cnt)  # 3298
 
 cnt = 0
-search('start', paths, [], 0, ['start'])
+search('start', [], 0, ['start'])
 print(cnt)  # 93572
