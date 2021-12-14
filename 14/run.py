636964900314e7604985f87008e5907ec28816ca
Ciprian Grigore <grigoresc@outlook.com>
2021-12-14 22:25:56 +0200

    day 14

--- 14/run.py
+++ 14/run.py
@@ -1,7 +1,6 @@
-from collections import Counter
-from functools import reduce
+from collections import Counter, defaultdict
 
-inp = [line.strip() for line in open('sample.txt', 'r')]
+# inp = [line.strip() for line in open('sample.txt', 'r')]
 inp = [line.strip() for line in open('input.txt', 'r')]
 
 
@@ -18,51 +17,47 @@ def parse(inp):
     return template, r
 
 
-t, r = parse(inp)
+def Part1():
+    t, r = parse(inp)
+    for i in range(10):
+        nt = ''
+        for idx, e in enumerate(t):
+            if idx > 0:
+                f = "" + t[idx - 1] + t[idx]
+                nt += r[f]
+            nt += e
+        t = nt
+    c = Counter(list(t))
+    mc = c.most_common()[0]
+    lc = c.most_common()[-1]
+    s1 = mc[1] - lc[1]
+    print(s1)  # 3284
 
-for i in range(10):
-    # print(i)
-    nt = ''
-    for idx, e in enumerate(t):
-        if idx > 0:
-            f = "" + t[idx - 1] + t[idx]
-            nt += r[f]
-        nt += e
-    t = nt
 
-c = Counter(list(t))
-mc = c.most_common()[0]
-lc = c.most_common()[-1]
-s1 = mc[1] - lc[1]  # 3284
-# t1 = t
-#
-# h = dict()
-#
-#
-# def expand(start, end, times):
-#     mid = r[start + end]
-#     if (times == 1):
-#         return start + mid
-#     left = expand(start, mid, times - 1)
-#     right = expand(mid, end, times - 1)
-#     res = left[1:-1] + right[1:-1]
-#     return res
-#
-#
-# t, r = parse(inp)
-# for i in range(1):
-#     # print(i)
-#     nt = ''
-#     for idx, e in enumerate(t):
-#         if idx > 0:
-#             f = expand(t[idx - 1], t[idx], 11)
-#             nt += f
-#         nt += e
-#     t = nt
-#
-# c = Counter(list(t))
-# mc2 = c.most_common()[0]
-# lc2 = c.most_common()[-1]
-# s2 = mc2[1] - lc2[1]  # 3284
-# t2 = t
-# print(len(t1), len(t2))
+def Part2():
+    t, r = parse(inp)
+    pairs = defaultdict(int)
+    cnts = defaultdict(int)
+    for i in t:
+        cnts[i] += 1
+    for i in range(len(t) - 1):
+        pairs[t[i] + t[i + 1]] = 1
+    for _ in range(40):
+        npairs = defaultdict(int)
+        for k, v in pairs.items():
+            mid = r[k[0] + k[1]]
+            cnts[mid] += v
+            left = k[0] + mid
+            right = mid + k[1]
+            npairs[left] += v
+            npairs[right] += v
+        pairs = npairs
+    c = Counter(cnts)
+    mc = c.most_common()[0]
+    lc = c.most_common()[-1]
+    s2 = mc[1] - lc[1]
+    print(s2)  # 4302675529689
+
+
+Part1()
+Part2()
